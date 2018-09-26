ddf2b094011e3e4c2cda9c627df90f463a5ec304
Marcelo Kath <marcelokathmk@gmail.com>
2018-09-26 19:22:04 -0300

    Implementação do onChange, para mudança de estado do componente.

--- src/template/iconButton.jsx
+++ src/template/iconButton.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
+import IF from './if'
 
-export default props => {
-    if  (props.hide){
-        return null
-    }else{
-        return (
-            <button className={'btn btn-'+props.style} onClick={props.onClick}>
-                <i className={'fa fa-'+props.icon}></i>
-            </button>
-        )
-    }
-}
+export default props => (
+    <IF test={!props.hide}>
+        <button className={'btn btn-'+props.style} onClick={props.onClick}>
+            <i className={'fa fa-'+props.icon}></i>
+        </button>
+    </IF>
+)
--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -4,11 +4,27 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+    
+    constructor(props){
+        super(props)
+        this.state = {description: '', list: [] }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleAdd(){
+        console.log(this.state.description)
+    }
+
+    handleChange(e){
+        this.setState({...this.state, description: e.target.value})
+    }
+    
     render(){
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm />
+                <TodoForm handleAdd={this.handleAdd} description={this.state.description} handleChange={this.handleChange} />
                 <TodoList />
             </div>
         )       
