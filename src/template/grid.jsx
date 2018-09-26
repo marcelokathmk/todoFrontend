cde67ce87e1b6ac994c45d5c9569367ab50cad1c
Marcelo Kath <marcelokathmk@gmail.com>
2018-09-26 18:35:25 -0300

    Criação dos componentes Grid e IconButton.

---
+++ src/template/grid.jsx
@@ -0,0 +1,33 @@
+import React, {Component} from 'react'
+
+export default class Grid extends Component {
+    
+    toCssClasses(numbers){
+        const col = numbers ? numbers.split(' ') : []
+        let classes = ''
+
+        if  (col[0]){
+            classes += `col-xs-${col[0]}`    
+        }
+        if  (col[1]){
+            classes += ` col-sm-${col[1]}`    
+        }
+        if  (col[2]){
+            classes += ` col-md-${col[2]}`    
+        }
+        if  (col[3]){
+            classes += ` col-lg-${col[3]}`    
+        }
+
+        return classes
+    }
+
+    render(){
+        const gridClasses = this.toCssClasses(this.props.cols || 12)
+        return (
+            <div className={gridClasses}>
+                {this.props.children}
+            </div>
+        )
+    }
+}
